c95d246bbf180c87a6098e3e41429c1574eb4ab5
gil <gil@MacBook-Pro-de-gil.local>
2016-09-05 17:05:06 -0600

    var_ar_log

---
+++ variables_ar_log.rb
@@ -0,0 +1,9 @@
+entero = 12
+float = 9.7
+float_2 = 2.765
+bool = 7 < 5
+bool_1 = 5 <= 3
+bool_2 = 28 != 673
+puts "Variable tipo string"
+variable = {"2" => "rojo", "3" => "azul", "3" => "verde"}
+p variable = ["Hola", "Dany", "saludo", "mapa"]
